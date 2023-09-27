984c5db8a687115cd2ee2291f2a2d0c7f1708344
Максим <verstak80@yandex.ru>
2023-09-27 09:53:55 +0300

    Add project files.

---
+++ UserManagement.Domain/Models/UserModelCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersManagement.Domain.Models
+{   
+    /// <summary>
+    /// В эту модель мы принимаем заполненные пользователем поля с фронта
+    /// </summary>
+    public class UserModelCreate
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int RoleId { get; set; }
+
+    }
+}
